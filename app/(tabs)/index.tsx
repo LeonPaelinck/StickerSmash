e4cc4b95d136689bed32d14a03b4e8bd6860d638
Leon Paelinck <leon.paelinck@gmail.com>
2025-12-03 13:39:39 +0100

    Chapter 3

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,21 @@
-import { Link } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+
+import Button from "@/components/Button";
+import ImageViewer from '@/components/ImageViewer';
+
+const PlaceholderImage = require('@/assets/images/background-image2.jpeg');
+
 
 export default function Index() {
   return (
-        <View style={styles.container}>
-      <Text style={styles.text}>Hello Luc!</Text>
-      <Link href="/about" style={styles.button}>Go to about screen</Link>
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage}/>
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Choose a photo" theme="primary" />
+        <Button label="Use this photo" />
+      </View>
     </View>
   );
 }
@@ -23,5 +33,17 @@ const styles = StyleSheet.create({
   button: {
     fontSize: 20,
     color: '#914343ff',
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
